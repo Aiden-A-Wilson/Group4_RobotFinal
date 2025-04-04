fdada456188bce076aff5db5e9a063e1575d069a
Harris Ibrahimi <98238342+SUP3RBOB@users.noreply.github.com>
2025-04-04 07:59:55 -0400

    Completed tests
    - 95% coverage

--- Group4_RobotFinal/Packet_Tests/Packet_Tests.cpp
+++ Group4_RobotFinal/Packet_Tests/Packet_Tests.cpp
@@ -10,7 +10,7 @@ namespace PacketTests
 	{
 	public:
 		
-		TEST_METHOD(SetCmd_SetsTheCommandType)
+		TEST_METHOD(SetCmd_WithDrive_SetsTheCommandType)
 		{
 			// Arrange
 			PktDef pkt = PktDef();
@@ -18,6 +18,45 @@ namespace PacketTests
 			
 			// Act
 			pkt.SetCmd(DRIVE);
+
+			// Assert
+			Assert::AreEqual((int)expectedCmd, (int)pkt.GetCmd());
+		}
+
+		TEST_METHOD(SetCmd_WithResponse_SetsTheCommandType)
+		{
+			// Arrange
+			PktDef pkt = PktDef();
+			enum CmdType expectedCmd = RESPONSE;
+
+			// Act
+			pkt.SetCmd(RESPONSE);
+
+			// Assert
+			Assert::AreEqual((int)expectedCmd, (int)pkt.GetCmd());
+		}
+
+		TEST_METHOD(SetCmd_WithSleep_SetsTheCommandType)
+		{
+			// Arrange
+			PktDef pkt = PktDef();
+			enum CmdType expectedCmd = SLEEP;
+
+			// Act
+			pkt.SetCmd(SLEEP);
+
+			// Assert
+			Assert::AreEqual((int)expectedCmd, (int)pkt.GetCmd());
+		}
+
+		TEST_METHOD(SetCmd_WithInvalidOption_ReturnsNegatveOne)
+		{
+			// Arrange
+			PktDef pkt = PktDef();
+			enum CmdType expectedCmd = (CmdType)-1;
+
+			// Act
+			pkt.SetCmd((CmdType)99);
 
 			// Assert
 			Assert::AreEqual((int)expectedCmd, (int)pkt.GetCmd());
@@ -70,6 +109,41 @@ namespace PacketTests
 			// Assert
 			int totalSize = HEADERSIZE + pkt.GetLength() + sizeof(unsigned char);
 			Assert::IsTrue(pkt.CheckCRC(pkt.GenPacket(), totalSize));
+		}
+
+		TEST_METHOD(GetAck_WithNoAcknowledgement_ReturnsZero) {
+			// Arrange
+			PktDef pkt = PktDef();
+
+			// Act
+			bool actual = pkt.GetAck();
+
+			// Assert
+			Assert::IsFalse(actual);
+		}
+
+		TEST_METHOD(Constructor_WithParameter_FillsPacket) {
+			// Arrange
+			PktDef pkt = PktDef();
+			struct PktDef::DriveBody body = { 0 };
+			body.Direction = 1;
+			body.Duration = 10;
+			body.Speed = 80;
+			pkt.SetCmd(DRIVE);
+			pkt.SetBodyData((char*)&body, sizeof(body));
+
+			// Act
+			PktDef newPkt = PktDef(pkt.GenPacket());
+			
+			// Assert
+			int size = HEADERSIZE + newPkt.GetLength() + sizeof(unsigned char);
+			struct PktDef::DriveBody actual = { 0 };
+			memcpy(&actual, newPkt.GetBodyData(), sizeof(actual));
+			Assert::AreEqual((int)DRIVE, (int)newPkt.GetCmd());
+			Assert::AreEqual((unsigned char)1, actual.Direction);
+			Assert::AreEqual((unsigned char)10, actual.Duration);
+			Assert::AreEqual((unsigned char)80, actual.Speed);
+			Assert::IsTrue(newPkt.CheckCRC(newPkt.GenPacket(), size));
 		}
 	};
 }
